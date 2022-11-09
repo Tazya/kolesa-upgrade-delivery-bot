c5e84d586cff78ac36bcb1a472d84e9c2216620a
Koala610 <vladli2000610@gmail.com>
2022-11-10 00:25:44 +0600

    Initial structure + basic dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module kolesa-upgrade-team/delivery-bot
 
 go 1.19
+
+require (
+	github.com/BurntSushi/toml v1.2.1
+	gopkg.in/telebot.v3 v3.1.2
+)
--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"kolesa-upgrade-team/delivery-bot/bot"
+	"log"
 
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Env      string
+	BotToken string
+	Dsn      string
+}
+
+func main() {
+	configPath := flag.String("config", "", "Path to config file")
+	flag.Parse()
+
+	cfg := &Config{}
+	_, err := toml.DecodeFile(*configPath, cfg)
+
+	if err != nil {
+		log.Fatalf("Ошибка декодирования файла конфигов %v", err)
+	}
+
+	upgradeBot := bot.UpgradeBot{
+		Bot: bot.InitBot(cfg.BotToken),
+	}
+
+	upgradeBot.Bot.Handle("/hello", upgradeBot.HelloHandler)
+	upgradeBot.Bot.Start()
 }
